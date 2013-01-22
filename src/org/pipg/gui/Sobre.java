9d483c80fc30d2cff307689224f42db324162a81
delki8 <delki8@gmail.com>
2013-01-22 06:55:02 -0200

    versão 1.0.0 finalizada, publicação em breve

--- src/org/pipg/gui/Sobre.java
+++ src/org/pipg/gui/Sobre.java
@@ -2,20 +2,15 @@ package org.pipg.gui;
 
 import org.pipg.R;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.widget.TextView;
+import android.view.View;
 
-public class Sobre extends FragmentActivity {
+public class Sobre extends Activity {
+
 	@Override
-	public void onCreate(Bundle icicle) {
-		super.onCreate(icicle);
-		setContentView(R.layout.sobre);
-
-		// Tratando informação sobre desenvolvedores
-		TextView txtDev = (TextView) findViewById(R.id.sobre_dev);
-		String dev = getString(R.string.dev_1) + " e "
-				+ getString(R.string.dev_2);
-		txtDev.setText(dev);
+	public void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.sobre_fragment);
+		super.onCreate(savedInstanceState);
 	}
 }
